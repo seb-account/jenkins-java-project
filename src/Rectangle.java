8f42dd6e3957f0e9afae25ba7361fc2e4cbdad4a
Sebastian R <sebray1234@gmail.com>
2018-02-12 17:48:11 +0100

    Initiate project(with bad practices) only for demo purposes

---
+++ src/Rectangle.java
@@ -0,0 +1,27 @@
+
+public class Rectangle {
+	private int length;
+	private int width;
+	
+	public Rectangle(int length,int width){
+		this.length=length;
+		this.width=width;
+	}
+
+	public int getArea(){
+		return length*width;
+	}
+
+	public int getPerimeter(){
+		return 2 * (length+width);
+	}
+
+	public int getLength(){
+		return length;
+	}
+
+	public int getWidth(){
+		return width;
+	}
+
+}
